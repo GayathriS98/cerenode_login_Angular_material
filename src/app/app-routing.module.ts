99923d934a6e130dc92df4558cee9585389902ba
Gayathri S <gayathrisankar798@gmail.com>
2020-03-15 19:29:40 +0530

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule,CanActivate } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { LoginComponent } from './login/login.component';
+import {AuthGuard} from './auth.guard'
+import { BarchartComponent } from './barchart/barchart.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:LoginComponent
+  },
+  
+  {
+    path:'home',
+    component:PostsComponent,
+    canActivate:[AuthGuard]
+  },
+  {
+    path:'home/dashboard',
+    component:BarchartComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FormGroup,FormControl,Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { from } from 'rxjs';
 
+import { Observable } from 'rxjs';
+import { DataModel } from './barchart/barchart.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngSocialMedia';
+
+  constructor(private http: HttpClient) {
+    localStorage.clear();
+  }
+  
+
 }
